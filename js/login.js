642319081ca17a49e7fc27eadc788191d84ff21c
cauarojas <cauarojas@hotmail.com>
2023-05-17 09:49:58 -0300

    pagina de sucesso criada

--- js/login.js
+++ js/login.js
@@ -5,30 +5,50 @@
 // const btnSubmit = document.getElementById('btnSubmit')
 // const btnLimpar = document.getElementById('btnLimpar')
 
-const tokenGerado = () => Math.random().toString(16).substring(2)
-
-const user1 = {
-    name: 'dede',
-    password: '12345',
-    gravarDados: true,
-    token: tokenGerado(),
-}
-const user2 = {
-    name: 'gege', // cspell: disable-line
-    password: '54321',
-    gravarDados: true,
-    token: tokenGerado(),
-}
-const users = [user1, user2]
+// const listaUsuarios = [
+//     //cspell: ignore denden usuario usuarios gege gersu mary
+//     {
+//         nomeCompleto: 'Denden da silva',
+//         nomeUsuario: 'denden',
+//         senhaUsuario: '123456',
+//     },
+//     {
+//         nomeCompleto: 'Gersu da silva',
+//         nomeUsuario: 'gege',
+//         senhaUsuario: '123456',
+//     },
+//     {
+//         nomeCompleto: 'José da silva',
+//         nomeUsuario: 'Jose',
+//         senhaUsuario: '123456',
+//     },
+//     {
+//         nomeCompleto: 'Paulo das Couves',
+//         nomeUsuario: 'paulo',
+//         senhaUsuario: '123456',
+//     },
+//     {
+//         nomeCompleto: 'Mary Help',
+//         nomeUsuario: 'mary',
+//         senhaUsuario: '123456',
+//     },
+//     {
+//         nomeCompleto: 'Pedro Silva',
+//         nomeUsuario: 'pedro',
+//         senhaUsuario: '123456',
+//     },
+// ]
+// localStorage.setItem('listaUser', JSON.stringify(listaUsuarios))
 addEventListener('click', (event) => {
     const inputUser = document.querySelector('#idUser')
     const inputPass = document.querySelector('#idPass')
     if (event.target.id == 'btnSubmit') {
         try {
+            const users = JSON.parse(localStorage.getItem('listaUser'))
             users.forEach((user) => {
                 if (
-                    inputUser.value == user.name &&
-                    inputPass.value == user.password
+                    inputUser.value == user.nomeUsuario &&
+                    inputPass.value == user.senhaUsuario
                 ) {
                     throw 'VALIDADO'
                 }
@@ -40,6 +60,7 @@ addEventListener('click', (event) => {
                 msgError.style.color = '#00ff00'
                 msgError.innerHTML =
                     '<span><strong>Usuário validado com sucesso</strong></span>'
+                window.location.href = './sucesso.html'
             } else {
                 msgError.style.color = '#ff0000'
                 msgError.innerHTML =
